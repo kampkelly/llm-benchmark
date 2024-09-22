65a5e01c7e7caad2a38263cab809ca5acb3ea894
Runor Adjekpiyede <kampkellykeys@gmail.com>
2024-09-22 23:13:33 +0100

    feat: seucre route with api key

--- metric_benchmark/apis/v1/route_benchmark.py
+++ metric_benchmark/apis/v1/route_benchmark.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, Depends, status
 from metric_benchmark.apis.benchmark_service import BenchmarkService
+from metric_benchmark.apis.auth import verify_api_key
 
 router = APIRouter()
 
 
 @router.get("/rankings", status_code=status.HTTP_200_OK)
-def get_database_info(benchmark_service: BenchmarkService = Depends(BenchmarkService)):
+def get_database_info(
+    benchmark_service: BenchmarkService = Depends(BenchmarkService),
+    api_key: str = Depends(verify_api_key)
+):
     response = benchmark_service.get_simulation_and_rankings()
     return response
